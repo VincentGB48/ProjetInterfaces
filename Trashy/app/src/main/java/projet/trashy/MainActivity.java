3dfdd1954daf1a6fac6f3dedd75d92ff7e7db522
VincentGB48 <VincentGB48@hotmail.ca>
2017-04-15 14:36:43 -0400

    AnalyseLine.java

---
+++ Trashy/app/src/main/java/projet/trashy/MainActivity.java
@@ -0,0 +1,45 @@
+package projet.trashy;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //creation du drawer layout
+        mToolbar = (Toolbar) findViewById(R.id.nav_action); //pour merge le layout avec la toolbar(navigation action)
+        setSupportActionBar(mToolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);//drawer layout(navigation_menu, navigation_header)
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //
+    }
+
+    @Override //sort le menu à l'appui du bouton en haut a gauche
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
